cf6e5b0fcc3a51f3e0b7efa3d717231550a180d0
nadianawaz <Manha1011!>
2022-04-14 11:17:14 +0100

    Initial commit3

--- src/day39_Recap/shapeTask/Square.java
+++ src/day39_Recap/shapeTask/Square.java
@@ -1,10 +1,26 @@
 package day39_Recap.shapeTask;
 
-public class Square extends Shape{
+public class Square extends Shape {
 
-    public Square(String name) {
-        super(name);
+    private double side;
+
+    public double getSide() {
+        return side;
     }
+
+    public void setSide(double side) {
+        if (side <= 0){
+            System.err.println("Invalid Side: "+side);
+            System.exit(1);
+        }
+        this.side = side;
+    }
+
+    public Square(String name, double side) {
+        super("Square");
+        setSide(side);
+    }
+
 }
 /*
 Square extends Shape:
